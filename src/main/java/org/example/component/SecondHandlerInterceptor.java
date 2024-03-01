5c604c75279b8a083fe2b07203d67cd3219273a6
huangyanduo <1050510316@qq.com>
2024-03-01 10:05:14 +0800

    测试HandlerInterceptor

---
+++ src/main/java/org/example/component/SecondHandlerInterceptor.java
@@ -0,0 +1,33 @@
+package org.example.component;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author huang
+ */
+@Component
+public class SecondHandlerInterceptor implements HandlerInterceptor {
+    private final Logger logger = LoggerFactory.getLogger(SecondHandlerInterceptor.class);
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (handler instanceof HandlerMethod) {
+            logger.info("second-preHandle当前接口：{}", ((HandlerMethod) handler).getMethod().getName());
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        if (handler instanceof HandlerMethod) {
+            logger.info("second-postHandle当前接口：{}", ((HandlerMethod) handler).getMethod().getName());
+        }
+    }
+}
---
+++ src/main/java/org/example/component/WebAppConfigurer.java
@@ -0,0 +1,30 @@
+package org.example.component;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author huang
+ */
+@Configuration
+public class WebAppConfigurer implements WebMvcConfigurer {
+    private FirstHandlerInterceptor firstHandlerInterceptor;
+
+    private SecondHandlerInterceptor secondHandlerInterceptor;
+
+    @Autowired
+    public WebAppConfigurer(FirstHandlerInterceptor firstHandlerInterceptor,
+                            SecondHandlerInterceptor secondHandlerInterceptor) {
+        this.firstHandlerInterceptor = firstHandlerInterceptor;
+        this.secondHandlerInterceptor = secondHandlerInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 加入顺序就是执行顺序
+        registry.addInterceptor(firstHandlerInterceptor).addPathPatterns("/**");
+        registry.addInterceptor(secondHandlerInterceptor).addPathPatterns("/**");
+    }
+}
